103ea09aeeb39aa8b1e130f42f7532e6a7c9b1ae
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2024-12-11 15:36:46 +0900

    Day 11

---
+++ day11/day11a.py
@@ -0,0 +1,30 @@
+"""
+--- Day 11: Plutonian Pebbles ---
+https://adventofcode.com/2024/day/11
+"""
+
+from functools import cache
+from math import floor, log10
+
+@cache
+def process(n: int) -> int | tuple[int, int]:
+    if n == 0: return 1
+    digits = floor(log10(n)) + 1
+    if not digits&1: return divmod(n, 10**(digits//2))
+    return n*2024
+
+@cache
+def iterate(stone: int, i: int) -> int:
+    if i == 0: return 1
+    value = process(stone)
+    if isinstance(value, tuple):
+        return iterate(value[0], i - 1) + iterate(value[1], i - 1)
+    else:
+        return iterate(value, i - 1)
+    
+with open("input.txt") as f:
+    line = map(int, f.read().split())
+
+print(sum(iterate(stone, 25) for stone in line))
+
+#Total Stones: 231278
